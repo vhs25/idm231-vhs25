3f07c11d32c477b55574beba62ed7ebb27d70137
Victoria Stauffer <vstauffer21@gmail.com>
2020-02-20 15:43:10 -0500

    trying modal js...

--- code/main.js
+++ code/main.js
@@ -1,4 +1,4 @@
-const button = document.getElementsByClassName('card');
+/*const buttons = document.getElementsByClassName('card');
 const popup = document.getElementsByClassName('pop-up');
 
 const openModal = ()=>{
@@ -13,3 +13,20 @@ console.log(button);
 
 button.addEventListener('click', openModal, false);
 popup.addEventListener('click', closeModal, false);
+**/
+const popup = document.getElementsByClassName('pop-up')
+
+const handleButtonClick = sign=>{
+    if(sign === aries){
+        popup.title = The Magician
+    }
+}
+
+
+
+
+const buttons = document.getElementsByClassName('card');
+
+buttons.forEach('click', e => {
+    
+});
