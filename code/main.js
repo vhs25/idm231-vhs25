8d545f46443b71d01a04c0c402bc2153dd9a2293
Victoria Stauffer <vstauffer21@gmail.com>
2020-02-26 16:20:48 -0500

    Content done!

--- code/main.js
+++ code/main.js
@@ -40,7 +40,32 @@ function cardInfo(){
     else if(id='libra'){
         p_i.src = ""
         p_h.innerHTML = "Temperance"
-        p_p.innerHTML = ""
+        p_p.innerHTML = "Temperence represents a peaceful and cooperative person. Their patience and cool temperment allows them to be a moderator and display calmness while under stress."
+    }
+    else if(id='scorpio'){
+        p_i.src = ""
+        p_h.innerHTML = "The Hermit"
+        p_p.innerHTML = "The Hermit represents a calm and meditative spirit. These people, through solitue and stillness, have the ability to look inward for answers and guidance in their life."
+    }
+    else if(id='sag'){
+        p_i.src = ""
+        p_h.innerHTML = "Judgement"
+        p_p.innerHTML = "The Judgement card shows a person who is forgiving and wise. People of the Judgement card value honesty and move boldly through life. You are encouraged to find comfort in who you are and in the wisdom you hold from life's experience."
+    }
+    else if(id='aquarius'){
+        p_i.src = ""
+        p_h.innerHTML = "The Fool"
+        p_p.innerHTML = "The Fool card represents spontaneity and and innocence. These people are commited to following their heart, even in the face of the unknown. This boldness can be seen as naive, but is really just a way of being free."
+    }
+    else if(id='pisces'){
+        p_i.src = ""
+        p_h.innerHTML = "Strength"
+        p_p.innerHTML = "The Strength card represents people who have a strength of heart that empowers them to live confidently and compasionately. These people commit to their values and show persistece for what they know they need."
+    }
+    else if(id='capricorn'){
+        p_i.src = ""
+        p_h.innerHTML = "The Emperor"
+        p_p.innerHTML = "The Emeror card represents stability and decisiveness. These people have a clear vision of who they are and what they want. Their inner strength is backed by their principles and values."
     }
 }
 
