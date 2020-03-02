83b2d10e419d02da275ad804654fc284cb6f713c
Victoria Stauffer <vstauffer21@gmail.com>
2020-03-02 18:40:38 -0500

    update

--- code/main.js
+++ code/main.js
@@ -74,6 +74,7 @@ const handleButtonClick = cardPick =>{
     console.log(cardPick);
     console.log(cardPick.target);
     console.log(cardPick.target.id);
+    console.log(cardInfo.img);
     popup.hidden = false;
     const feedback = document.querySelector('p');
     // feedback.innerHTML = message;
@@ -88,6 +89,33 @@ buttons.forEach(button =>{
 });
 
 buttons.onclick = function() {
-    popup.style.display = "block";
+    popup.style.display  = "block";
   }
 
+/* ZODIAC
+if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
+  AstroSign = "Capricorn";
+} else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
+  AstroSign = "Sag";
+} else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
+  AstroSign = "Scorpio";
+} else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
+  AstroSign = "Libra";
+} else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
+  AstroSign = "Virgo";
+} else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
+  AstroSign = "Leo";
+} else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
+  AstroSign = "Cancer";
+} else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
+  AstroSign = "Gemini";
+} else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
+  AstroSign = "Taurus";
+} else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
+  AstroSign = "Aries";
+} else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
+  AstroSign = "Pisces";
+} else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
+  AstroSign = "Aquarius";
+}
+**/
